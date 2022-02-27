8be0a3fdea611d1bbb4fe970e9d4c4401051c594
Yousef-FJ <yousef.fawzy.2000@gmail.com>
2022-02-28 00:12:35 +0300

    Make Item photo path nullable

--- Src/OnlineAuctionBackend/AuctionBackend.Application/Models/Item.cs
+++ Src/OnlineAuctionBackend/AuctionBackend.Application/Models/Item.cs
@@ -4,22 +4,20 @@
     {
         private User? _owner;
 
-        public Item(string name, double startingPrice, int ownerId, string photoUrl)
+        public Item(string name, double startingPrice)
         {
             Name = name;
             StartingPrice = startingPrice;
-            OwnerId = ownerId;
-            PhotoUrl = photoUrl;
         }
 
         public string Name { get; set; }
         public double StartingPrice { get; set; }
-        public string PhotoUrl { get; set; }
+        public string? PhotoUrl { get; set; }
         public Auction? Auction { get; private set; }
         public int OwnerId { get; set; }
         public User Owner
         {
-            private set => _owner = value;
+            set => _owner = value;
             get => _owner ??
                 throw new InvalidOperationException("Uninitialized property: " + nameof(Owner));
         }
