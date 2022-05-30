ae1d852d2b89bc5230bdd2700897d7fad868003f
Yousef-FJ <yousef.fawzy.2000@gmail.com>
2022-05-30 18:45:18 +0300

    Add page count and current page to paged response

--- Src/OnlineAuctionBackend/AuctionBackend.API/RemoteSchema/PagedResponse.cs
+++ Src/OnlineAuctionBackend/AuctionBackend.API/RemoteSchema/PagedResponse.cs
@@ -5,5 +5,7 @@ namespace AuctionBackend.Api.RemoteSchema
     public record PagedResponse<TPagedList>(TPagedList Data) where TPagedList : IPagedList
     {
         public int TotalCount { get; } = Data.TotalItemCount;
+        public int PageCount { get; } = Data.PageCount;
+        public int CurrentPage { get; } = Data.PageNumber;
     };
 }
